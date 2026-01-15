d882807c26b431af80d00b5acaf98e0973a14cb4
Eva <user@users-MacBook-Air.local>
2026-01-15 17:25:49 +0100

    cpp

--- Module04/ex03/IMateriaSource.hpp
+++ Module04/ex03/IMateriaSource.hpp
@@ -4,9 +4,7 @@
 
 class IMateriaSource {
 	public:	
-		IMateriaSource();
-		IMateriaSource(const IMateriaSource& other);
-		IMateriaSource& operator=(const IMateriaSource& other);		virtual ~IMateriaSource() {}
+		virtual ~IMateriaSource() {}
 		virtual void learnMateria(AMateria*) = 0;
 		virtual AMateria* createMateria(std::string const & type) = 0;
 };
